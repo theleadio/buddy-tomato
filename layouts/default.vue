791f71d09ac34b8b1b38aaaf6656f77c08d80591
Edmund Hee <edmund.hee05@gmail.com>
2020-05-18 15:19:55 +0800

    Updated page name and favicon

--- layouts/default.vue
+++ layouts/default.vue
@@ -15,7 +15,17 @@ export default {
     components:{
         naviWrapper: NaviWrapper,
         footerWrapper: FooterWrapper
-    }
+    },
+    head () {
+      return {
+        title: "Focus",
+        meta: [
+          // hid is used as unique identifier. Do not use `vmid` for it as it will not work
+          { hid: 'description', name: 'description', content: 'Pomodoro By TheLead' }
+        ],
+        
+      }
+  }
   
 }
 </script>
