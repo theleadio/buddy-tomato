a1fb558717e80203504dc730022e0a998d2f6b1a
Edmund Hee <edmund.hee05@gmail.com>
2020-07-31 14:12:14 +0800

    Updated with social sharing

---
+++ components/items/FacebookShare.vue
@@ -0,0 +1,26 @@
+<template>
+    <ShareNetwork
+        class="py-2 px-4 mx-2 text-white font-sans font-semibold cursor-pointer"
+        style="background-color: rgb(24, 119, 242);"
+        network="facebook"
+        url="https://focus.thelead.io/"
+        :title="`Finished another my task: ${title}`"
+        description="A simple focus app that uses pomodoro technique. Take a rest after focus for 25 minutes"
+        quote="A simple focus app that uses pomodoro technique. Take a rest after focus for 25 minutes"
+        hashtags="leadofficial,worksmart"
+        >
+        <i class="fab fah fa-lg fa-facebook-f text-white"></i>
+        Share on Facebook
+    </ShareNetwork>
+</template>
+<script>
+export default {
+  // Nothing needed here
+  props: {
+      title:{
+        type: String,
+        default: "Pomodoro"
+      }
+  }
+}
+</script>
